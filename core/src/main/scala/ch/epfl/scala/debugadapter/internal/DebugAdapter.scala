bf54006f5b9e4092deab9c5c32c7d9e1189987c7
tdudzik <tomaszdudzik.web@gmail.com>
2021-06-16 14:36:19 +0200

    wip

--- core/src/main/scala/ch/epfl/scala/debugadapter/internal/DebugAdapter.scala
+++ core/src/main/scala/ch/epfl/scala/debugadapter/internal/DebugAdapter.scala
@@ -8,9 +8,7 @@ import com.microsoft.java.debug.core.{DebugSettings, IEvaluatableBreakpoint}
 import com.sun.jdi._
 import io.reactivex.Observable
 
-import java.net.URI
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.Path
 import java.util
 import java.util.Collections
 import java.util.concurrent.CompletableFuture
@@ -49,44 +47,44 @@ private[debugadapter] object DebugAdapter {
 
   object CompletionsProvider extends ICompletionsProvider {
     override def codeComplete(
-                               frame: StackFrame,
-                               snippet: String,
-                               line: Int,
-                               column: Int
-                             ): util.List[Types.CompletionItem] = Collections.emptyList()
+        frame: StackFrame,
+        snippet: String,
+        line: Int,
+        column: Int
+    ): util.List[Types.CompletionItem] = Collections.emptyList()
   }
 
   class EvaluationProvider(sourceLookUpProvider: ISourceLookUpProvider) extends IEvaluationProvider {
     override def isInEvaluation(thread: ThreadReference): Boolean = false
 
     override def evaluate(
-                           expression: String,
-                           thread: ThreadReference,
-                           depth: Int
-                         ): CompletableFuture[Value] = {
+        expression: String,
+        thread: ThreadReference,
+        depth: Int
+    ): CompletableFuture[Value] = {
       val frame = thread.frames().get(depth)
       Evaluator.evaluate(expression, thread, frame)(sourceLookUpProvider)
     }
 
     override def evaluate(
-                           expression: String,
-                           thisContext: ObjectReference,
-                           thread: ThreadReference
-                         ): CompletableFuture[Value] = ???
+        expression: String,
+        thisContext: ObjectReference,
+        thread: ThreadReference
+    ): CompletableFuture[Value] = ???
 
     override def evaluateForBreakpoint(
-                                        breakpoint: IEvaluatableBreakpoint,
-                                        thread: ThreadReference
-                                      ): CompletableFuture[Value] = ???
+        breakpoint: IEvaluatableBreakpoint,
+        thread: ThreadReference
+    ): CompletableFuture[Value] = ???
 
     override def invokeMethod(
-                               thisContext: ObjectReference,
-                               methodName: String,
-                               methodSignature: String,
-                               args: Array[Value],
-                               thread: ThreadReference,
-                               invokeSuper: Boolean
-                             ): CompletableFuture[Value] = ???
+        thisContext: ObjectReference,
+        methodName: String,
+        methodSignature: String,
+        args: Array[Value],
+        thread: ThreadReference,
+        invokeSuper: Boolean
+    ): CompletableFuture[Value] = ???
 
     override def clearState(thread: ThreadReference): Unit = {}
   }
